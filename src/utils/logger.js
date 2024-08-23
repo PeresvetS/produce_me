6cc4ed3197f68d122f686f5b1f94bbfa7c867ef9
Nikita Solovev <nik.writter@ya.ru>
2024-08-23 14:45:21 +0300

    add temprary logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -14,10 +14,10 @@ const logger = winston.createLogger({
   ]
 });
 
-if (process.env.NODE_ENV !== 'production') {
+// if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.simple()
   }));
-}
+// }
 
 module.exports = logger;
