f472a53772acb8012b60b924e33ed052024303d8
Nikita Solovev <nik.writter@ya.ru>
2024-09-01 08:53:05 +0300

    add logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,23 +1,66 @@
 // logger.js
 
 const winston = require('winston');
+const { combine, timestamp, printf, errors, splat } = winston.format;
+require('winston-daily-rotate-file');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
+
+// Создаем кастомный формат для логов
+const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
+  let msg = `${timestamp} [${level}] : ${message}`;
+  if (Object.keys(metadata).length > 0) {
+    msg += ` ${safeStringify(metadata)}`;
+  }
+  return msg;
 });
 
-// if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-// }
+// Конфигурация для ротации файлов с общими логами
+const fileRotateTransport = new winston.transports.DailyRotateFile({
+  filename: 'temp/logs/combined-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '14d',
+  format: combine(
+    timestamp(),
+    myFormat
+  )
+});
+
+// Конфигурация для ротации файлов с ошибками
+const errorFileRotateTransport = new winston.transports.DailyRotateFile({
+  filename: 'temp/logs/error-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '14d',
+  level: 'error',
+  format: combine(
+    timestamp(),
+    myFormat
+  )
+});
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: combine(
+    timestamp(),
+    errors({ stack: true }),
+    splat(),
+    myFormat
+  ),
+  defaultMeta: { service: 'tg-wa-service' },
+  transports: [
+    fileRotateTransport,
+    errorFileRotateTransport
+  ],
+});
+
+
+
+logger.add(new winston.transports.Console({
+  format: combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  )
+}));
 
 module.exports = logger;
